8948394cf4b8e94a84a0e7801ede80715c08c472
EC2 Default User <ec2-user@ip-172-31-32-41.ap-northeast-1.compute.internal>
2025-04-16 11:08:58 +0000

    ユーザーに関する機能2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/image/sample-author1.jpg')
-      profile_image.attach(io: File.onen(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(rasize_to_limit: [100,100]).processed
   end
 end
